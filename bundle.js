36a00875bf3c9012842686045c19a1a542d13306
bruno <bruno@bruno-N56VZ.(none)>
2015-05-15 12:39:06 +0100

    test

--- bundle.js
+++ bundle.js
@@ -9216,19 +9216,7 @@ var $ = require('jquery');
 console.log('AQUI');
 
 
-$.ajax({
-            url: "http://rest.pubmlst.org/db/pubmlst_spneumoniae_isolates/isolates/9",
-            type: "GET",
-
-            contentType: 'application/json; charset=utf-8',
-            success: function(resultData) {
-               console.log(resultData);
-
-            },
-            error : function(jqXHR, textStatus, errorThrown) {
-            },
-
-            timeout: 120000,
-        });
-
+$.getJSON('http://rest.pubmlst.org/db/pubmlst_spneumoniae_isolates/isolates/9', function(d){
+	console.log(d);
+});
 },{"jquery":1}]},{},[2]);
--- index.js
+++ index.js
@@ -3,17 +3,6 @@ var $ = require('jquery');
 console.log('AQUI');
 
 
-$.ajax({
-            url: "http://rest.pubmlst.org/db/pubmlst_spneumoniae_isolates/isolates/9",
-            type: "GET",
-
-            contentType: 'application/json; charset=utf-8',
-            success: function(resultData) {
-               console.log(resultData);
-
-            },
-            error : function(jqXHR, textStatus, errorThrown) {
-            },
-
-            timeout: 120000,
-        });
+$.getJSON('http://rest.pubmlst.org/db/pubmlst_spneumoniae_isolates/isolates/9', function(d){
+	console.log(d);
+});
